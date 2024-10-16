60d37cdd9a73aa7159d28ba6577bfaee6f6c49dd
B612-jujin <jujingood@gmail.com>
2024-10-16 10:24:47 +0900

    자바 중간고사 대충 짠거

---
+++ src/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+
+    private String isbn;
+    private String BookName;
+    private String Author;
+    private String Publisher;
+    private int Price;
+
+}
---
+++ src/BookManagement.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class BookManagement {
+
+    public static void addBook(ArrayList<Book> books) {
+        books.add(new Book())
+
+    }
+    public static Book BookSelect(ArrayList<Book> books){
+
+        return books.get(0);
+    }
+    public static void SetBook(ArrayList<Book> books){
+
+    }
+    public static void DeleteBook(ArrayList<Book> books){
+
+    }
+}
+
+
